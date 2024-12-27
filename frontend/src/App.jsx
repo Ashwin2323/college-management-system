07dcf675b4403ee3bd5071e5458fc1106a1c99d0
Ashwin2323 <ashwinhammar5555@gmail.com>
2024-12-27 23:04:11 +0530

    Renamed and Restructured Files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+// Import other pages as needed
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        {/* Add more routes as needed */}
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
